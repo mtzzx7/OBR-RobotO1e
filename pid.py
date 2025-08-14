9b1eb42ef7d825ffa745930e2e2bc732c950e627
Matheus <matheusemanuel0567@gmail.com>
2025-08-14 18:33:43 -0300

    Modularização pt.2

--- pid.py
+++ pid.py
@@ -1,3 +1,35 @@
+# Funções PID e redefinir
+import asyncio
+from pybricks.tools import wait
+
+async def redefinir_pid(state):
+    state['erro_final'] = 0
+    state['integral'] = 0
+    state['derivada'] = 0
+    state['correcao'] = 0
+    state['erro'] = 0
+    await wait(0)
+
+async def PID(kp, ki, kd, state):
+    state['integral'] += state['erro']
+    state['derivada'] = state['erro'] - state['erro_final']
+    state['correcao'] = state['erro'] * kp + (state['integral'] * ki + state['derivada'] * kd)
+    state['erro_final'] = state['erro']
+    await wait(0)
+
+async def redefinir(state, MTZZ):
+    await wait(0)
+    state['erro_final'] = 0
+    state['integral'] = 0
+    state['derivada'] = 0
+    state['correcao'] = 0
+    state['erro'] = 0
+    state['method_stop'] = 0
+    state['pid_p'] = 3.30
+    state['pid_i'] = 0.002
+    state['pid_d'] = 0.016
+    state['guinadacalc'] = MTZZ.imu.tilt()[1]
+    state['guinadarefmove'] = MTZZ.imu.tilt()[1]
 # Funções e classes para controle PID
 
 class PID:
--- sensores.py
+++ sensores.py
@@ -1,3 +1,27 @@
+# Funções de sensores e cor
+def is_prata(hsv):
+    h, s, v = hsv
+    return 205 <= h <= 215 and s <= 30 and v >= 70
+
+def is_verde(hsv):
+    h, s, v = hsv
+    return 95 <= h <= 165 and s >= 50 and v >= 40
+
+def is_brancofrente(hsv):
+    h, s, v = hsv
+    return 195 <= h <= 200 and s <= 15 and v <= 90
+
+def is_verdefrente(hsv):
+    h, s, v = hsv
+    return 150 <= h <= 165 and s <= 90 and v <= 35
+
+def is_vermelhofrente(hsv):
+    h, s, v = hsv
+    return 345 <= h <= 350 and s <= 95 and v <= 30
+
+def is_pretoresgate(hsv):
+    h, s, v = hsv
+    return 160 <= h <= 220 and s <= 30 and v <= 80
 # Funções relacionadas aos sensores
 # Exemplo: leitura de sensores, interpretação de cor, etc.
 
